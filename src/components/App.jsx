8933a9be2e7eab3786cdfddd855a8360442de6a6
Valik <valik3201@gmail.com>
2024-02-26 23:45:48 +0100

    feat: integrate useNavigate hook and navigate function with NextUIProvider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import Home from 'pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Contacts from '../pages/Contacts';
-
 import Navigation from './Navigation';
 
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login />} />
-        <Route path="contacts" element={<Contacts />} />
-        <Route path="*" element={<Home />} />
-      </Route>
-    </Routes>
+    <NextUIProvider navigate={navigate}>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route index element={<Home />} />
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="contacts" element={<Contacts />} />
+            <Route path="*" element={<Home />} />
+          </Route>
+        </Routes>
+      </NextThemesProvider>
+    </NextUIProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,19 +4,13 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './components/App';
-import { NextUIProvider } from '@nextui-org/react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <NextUIProvider>
-          <NextThemesProvider attribute="class" defaultTheme="dark">
-            <App />
-          </NextThemesProvider>
-        </NextUIProvider>
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
