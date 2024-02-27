626b9c5cf730e8dbc3ff815a787eddb084bebaef
Valik <valik3201@gmail.com>
2024-02-28 00:32:33 +0100

    style: update margin footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <footer className="fixed bottom-0 left-0 z-20 w-full bg-white dark:bg-black p-4 border-t border-divider backdrop-blur-lg data-[menu-open=true]:backdrop-blur-xl backdrop-saturate-150 bg-background/70 md:py-6 px-6">
       <div className="md:flex md:items-center md:justify-between max-w-[1024px] mx-auto">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="text-sm text-gray-500 md:text-center dark:text-gray-400">
           © 2024 PhoneBook
         </span>
-        <ul className="flex flex-wrap font-light items-center gap-4 mt-3 text-sm sm:mt-0">
+        <ul className="flex flex-wrap font-light items-center gap-4 mt-3 text-sm md:mt-0">
           <li>
             <Link
               href="https://github.com/Valik3201/goit-react-hw-08-phonebook"
