312b7e86a6ee6544c8637263d949f5cb374e0bb1
Valik <valik3201@gmail.com>
2024-02-27 12:24:02 +0100

    feat: implement PrivateRoute and RestrictedRoute components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,63 @@
+import { useEffect, lazy } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import Home from 'pages/Home';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Contacts from '../pages/Contacts';
+import { refreshUser } from '../redux/auth/operations';
+import { useAuth } from 'hooks';
+import { PrivateRoute } from './PrivateRout';
+import { RestrictedRoute } from './RestrictedRoute';
 import Layout from './Layout';
 
-function App() {
-  const navigate = useNavigate();
+const Home = lazy(() => import('../pages/Home'));
+const Register = lazy(() => import('../pages/Register'));
+const Login = lazy(() => import('../pages/Login'));
+const Contacts = lazy(() => import('../pages/Contacts'));
 
-  return (
+const App = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
     <NextUIProvider navigate={navigate}>
       <NextThemesProvider attribute="class" defaultTheme="dark">
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="register" element={<Register />} />
-            <Route path="login" element={<Login />} />
-            <Route path="contacts" element={<Contacts />} />
+            <Route
+              path="register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<Register />}
+                />
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+              }
+            />
+            <Route
+              path="contacts"
+              element={
+                <PrivateRoute redirectTo="/login" component={<Contacts />} />
+              }
+            />
             <Route path="*" element={<Home />} />
           </Route>
         </Routes>
       </NextThemesProvider>
     </NextUIProvider>
   );
-}
+};
 
 export default App;
